14cae46aab4938ed9b687264aa48f0c9c346c2e1
Alex Prut <1648497+alexprut@users.noreply.github.com>
2015-05-07 14:07:24 +0200

    Singleton

---
+++ Singleton/src/Singleton.java
@@ -0,0 +1,17 @@
+class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {}
+
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+            Singleton.instance = new Singleton();
+        }
+
+        return instance;
+    }
+
+    protected String doSomething() {
+        return "I do something";
+    }
+}
---
+++ Singleton/src/Test.java
@@ -0,0 +1,7 @@
+class Test {
+    public static void main(String[] args) {
+        Singleton s = Singleton.getInstance();
+
+        System.out.println(s.doSomething());
+    }
+}
